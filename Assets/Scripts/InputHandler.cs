cfdd36de0933477b66db6266fc6008c9c44817b7
maxfrank91 <Maximilian.F1@gmx.de>
2015-08-31 21:19:02 +0200

    first commit

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public static GameObject draggedObject;
+
+	public enum UIColor {RED, GREEN, BLUE, COUNT};
+	public GameObject panel;
+
+	static int minMovement = 10;
+	Vector3 startPosition;
+	UIColor currentColor;
+	bool over = false;
+	Color lightBlue = new Color (0.5f, 0.5f, 1);
+	Color lightGreen = new Color (0.5f, 1, 0.5f);
+	Color lightRed = new Color (1, 0.5f, 0.5f);
+	float time;
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		startPosition = transform.position;
+	}
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		if ((startPosition - Input.mousePosition).magnitude > minMovement) 
+		{
+			draggedObject = gameObject;
+			transform.position = Input.mousePosition;
+		}
+
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		// click
+		if (draggedObject == null) 
+		{
+
+		}
+		draggedObject = null;
+	}
+
+	#endregion
+
+	#region IPointerEnterHandler implementation
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+		over = true;
+	}
+
+	#endregion
+
+	#region IPointerExitHandler implementation
+
+	public void OnPointerExit (PointerEventData eventData)
+	{
+		panel.SetActive(false);
+		over = false;
+		time = 0;
+	}
+
+	#endregion
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		changeColor();
+		time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (over)
+		{
+			time += Time.deltaTime;
+			if ( Input.GetMouseButtonUp(1)) 
+			{
+				currentColor = (UIColor)(((int)currentColor+1) % (int)UIColor.COUNT);
+				changeColor();
+			}
+			if (Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse Y") < 0 )
+			{
+				time = 0;
+				panel.SetActive(false);
+			}
+			if (time >= 0.5f) 
+			{
+				panel.SetActive(true);
+			}
+		}
+	}
+
+	void changeColor()
+	{
+		ColorBlock tempColors = GetComponent<Button>().colors;
+		switch(currentColor) {
+		case UIColor.BLUE:
+			tempColors.normalColor = Color.blue;
+			tempColors.highlightedColor = lightBlue;
+			break;
+		case UIColor.RED:
+			tempColors.normalColor = Color.red;
+			tempColors.highlightedColor = lightRed;
+			break;
+		case UIColor.GREEN:
+			tempColors.normalColor = Color.green;
+			tempColors.highlightedColor = lightGreen;
+			break;
+		}
+		GetComponent<Button>().colors = tempColors;
+	}
+}
+
+
+
+
+
+
+
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject buttonPrefab;
+	public GameObject menuButtonPrefab;
+	public GameObject UIMain;
+	public GameObject UIMenu;
+
+	public bool record = false;
+
+	string fileName;
+	
+	struct RecordData
+	{
+		GameObject go;
+		Event usedEvent;
+		float time;
+	};
+
+	List<GameObject> buttons = new List<GameObject>();
+	int buttoncount = 3;
+
+	List<RecordData> recData = new List<RecordData>();
+	List<GameObject> menuButtons = new List<GameObject>();
+	
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i = 0; i < buttoncount; i++)
+		{
+			GameObject button = Instantiate(buttonPrefab);
+			button.transform.position = new Vector3(250*(i+1), -200, 0);
+			button.GetComponent<RectTransform>().sizeDelta = new Vector2(Random.Range(100,200), Random.Range(50,150));
+			button.transform.parent = UIMain.transform;
+			buttons.Add(button);
+		}
+		readJSON();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void readJSON()
+	{
+		string jsonString = Resources.Load<TextAsset>("menu").text;
+		JSONObject j = new JSONObject(jsonString);
+		accessData(j);
+
+	}
+	void accessData(JSONObject obj)
+	{
+		switch(obj.type)
+		{
+		case JSONObject.Type.OBJECT:
+			for(int i = 0; i < obj.list.Count; i++){
+				string key = (string)obj.keys[i];
+				if (key == "button")
+				{
+					GameObject mbutton = Instantiate(menuButtonPrefab);
+					menuButtons.Add(mbutton);
+				}
+				JSONObject j = (JSONObject)obj.list[i];
+				Debug.Log(key);
+				accessData(j);
+			}
+			break;
+		case JSONObject.Type.STRING:
+			Debug.Log(obj.str);
+			break;
+		}
+	}
+}
